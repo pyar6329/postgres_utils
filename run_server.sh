45f3885cf14b29be4a8f151b0bea3af4537a99ef
Tsukasa Arima <pyar6329@gmail.com>
2020-05-31 12:47:14 +0000

    add run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+POSTGRES_DATA="${PWD}/data"
+
+if ! [ -e "${POSTGRES_DATA}" ]; then
+  mkdir -p data
+fi
+
+docker run -it \
+  -d \
+  --rm \
+  -p "${POSTGRES_PORT:-5432}:${POSTGRES_PORT:-5432}" \
+  -e "POSTGRES_DB=${POSTGRES_DATABASE:-dsf_api}" \
+  -e "POSTGRES_USER=${USER}" \
+  -e "POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}" \
+  -e "PGDATA=/data" \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -v "${POSTGRES_DATA}:/data" \
+  postgres:11
+
