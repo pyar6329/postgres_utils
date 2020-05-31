2d447fa6bfcd7cad30b5bb8d30c9c2efe8736da4
Tsukasa Arima <pyar6329@gmail.com>
2020-05-31 13:39:42 +0000

    add restore script

---
+++ restore.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+# set below environment variables
+# RESTORE_HOSTNAME="database hostname"
+# RESTORE_PASSWORD="database password"
+# RESTORE_PORT="database port"
+# RESTORE_DATABASE="database name"
+# RESTORE_USERNAME="database username"
+
+OUTPUT_DIR="${RESTORE_DATABASE}"
+
+case "$(uname -s)" in
+  ("Darwin") CPU_CORES=$(sysctl -n hw.ncpu);;
+  ("Linux") CPU_CORES=$(grep -c processor /proc/cpuinfo);;
+esac
+
+BEGIN_SECOND=${SECONDS}
+
+tar -I pzstd -xf ${OUTPUT_DIR}.tar.zst
+echo "extract is finished"
+
+PGPASSWORD=${RESTORE_PASSWORD} pg_restore \
+  -h ${RESTORE_HOSTNAME} \
+  -p ${RESTORE_PORT} \
+  -U ${RESTORE_USERNAME} \
+  -w \
+  -d ${RESTORE_DATABASE} \
+  -v \
+  -j ${CPU_CORES} \
+  -F d \
+  -x \
+  -O \
+  ${OUTPUT_DIR}
+
+echo "pg_restore is finished"
+
+END_SECOND=${SECONDS}
+TOTAL_SECOND=$(( ${END_SECOND} - ${BEGIN_SECOND} ))
+RESULT_HOUR=$(eval "echo $(date -ud "@${TOTAL_SECOND}" +'$((%s/3600/24)) days %H hours %M minutes %S seconds')")
+echo "total: ${RESULT_HOUR}"
