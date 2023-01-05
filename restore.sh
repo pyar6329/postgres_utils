e33803f0b2f961aeba550a360decdab04a83b1b8
Tsukasa Arima <pyar6329@gmail.com>
2023-01-05 06:33:14 +0000

    add ANALYZE VERBOSE

--- restore.sh
+++ restore.sh
@@ -55,3 +55,15 @@ END_SECOND=${SECONDS}
 TOTAL_SECOND=$(( ${END_SECOND} - ${BEGIN_SECOND} ))
 RESULT_HOUR=$(eval "echo $(date -ud "@${TOTAL_SECOND}" +'$((%s/3600/24)) days %H hours %M minutes %S seconds')")
 echo "total: ${RESULT_HOUR}"
+
+echo "run SQL: ANALYZE VERBOSE;"
+
+PGPASSWORD=${RESTORE_PASSWORD} psql \
+  -h ${RESTORE_HOSTNAME} \
+  -p ${RESTORE_PORT} \
+  -U ${RESTORE_USERNAME} \
+  -d ${RESTORE_DATABASE} \
+  -w \
+  -c "ANALYZE VERBOSE;"
+
+echo "completed run SQL: ANALYZE VERBOSE;"
