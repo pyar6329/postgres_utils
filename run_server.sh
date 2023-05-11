20c689accc04de9bee9477b542d1643f8ea3dea5
Tsukasa Arima <pyar6329@gmail.com>
2023-05-12 08:04:34 +0900

    modify script

--- run_server.sh
+++ run_server.sh
@@ -5,29 +5,53 @@ set -e
 POSTGRES_DATA="${PWD}/data"
 POSTGRES_INITDB="${PWD}/initdb"
 
+OS_NAME="$(uname -s)"
+
 if ! [ -e "${POSTGRES_DATA}" ]; then
   mkdir -p ${POSTGRES_DATA}
 fi
 
-docker run -it \
-  -d \
-  --rm \
-  --name "postgres" \
-  -p "${POSTGRES_PORT:-5432}:5432" \
-  -e "POSTGRES_DB=${POSTGRES_DATABASE:-dsf_api}" \
-  -e "POSTGRES_USER=${USER}" \
-  -e "POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}" \
-  -e "PGDATA=/data" \
-  -u $(id -u ${USER}):$(id -g ${USER}) \
-  -v /etc/passwd:/etc/passwd:ro \
-  -v /etc/group:/etc/group:ro \
-  -v "${POSTGRES_INITDB}:/docker-entrypoint-initdb.d" \
-  -v "${POSTGRES_DATA}:/data" \
-  postgres:14.5 postgres \
-  -c log_destination=stderr \
-  -c log_statement=all \
-  -c log_connections=on \
-  -c log_disconnections=on
+case ${OS_NAME} in
+  "Darwin" )
+    docker run -it \
+      -d \
+      --rm \
+      --name "postgres" \
+      -p "${POSTGRES_PORT:-5432}:5432" \
+      -e "POSTGRES_DB=${POSTGRES_DATABASE:-dsf_api}" \
+      -e "POSTGRES_USER=${USER}" \
+      -e "POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}" \
+      -e "PGDATA=/data" \
+      -v "${POSTGRES_INITDB}:/docker-entrypoint-initdb.d" \
+      -v "${POSTGRES_DATA}:/data" \
+      postgres:14.5 postgres \
+      -c log_destination=stderr \
+      -c log_statement=all \
+      -c log_connections=on \
+      -c log_disconnections=on
+    ;;
+  * )
+    docker run -it \
+      -d \
+      --rm \
+      --name "postgres" \
+      -p "${POSTGRES_PORT:-5432}:5432" \
+      -e "POSTGRES_DB=${POSTGRES_DATABASE:-dsf_api}" \
+      -e "POSTGRES_USER=${USER}" \
+      -e "POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}" \
+      -e "PGDATA=/data" \
+      -u $(id -u ${USER}):$(id -g ${USER}) \
+      -v /etc/passwd:/etc/passwd:ro \
+      -v /etc/group:/etc/group:ro \
+      -v "${POSTGRES_INITDB}:/docker-entrypoint-initdb.d" \
+      -v "${POSTGRES_DATA}:/data" \
+      postgres:14.5 postgres \
+      -c log_destination=stderr \
+      -c log_statement=all \
+      -c log_connections=on \
+      -c log_disconnections=on
+    ;;
+esac
 
 # log_destination: ログの出力先
 # log_statement: クエリをログに出力する
