b38692f34a7538c66fb810f62e8d0bc7fa1afe57
Tsukasa Arima <pyar6329@gmail.com>
2024-05-20 03:38:45 +0000

    add re-create_database

--- re_create_database.sh
+++ re_create_database.sh
@@ -76,6 +76,17 @@ function create_database {
     -c "create database ${target_db_name};"
 }
 
+function re_initialize_data {
+  PGPASSWORD=${RESTORE_PASSWORD} \
+  psql \
+    -h ${RESTORE_HOSTNAME} \
+    -p ${RESTORE_PORT} \
+    -U ${RESTORE_USERNAME} \
+    -d ${RESTORE_DATABASE} \
+    -w \
+    -c "create extension hypopg; create extension index_advisor cascade; create role postgres with login password 'postgres'; alter role postgres with superuser createrole createdb replication bypassrls; create role postgres_ro with login password 'postgres'; grant pg_read_all_data to postgres_ro;"
+}
+
 # This command is failed if connected database is not found
 if ! $(connected_db_name="${RESTORE_DATABASE}" run_some_query > /dev/null 2>&1); then
   echo "This command cannot access to database (${RESTORE_DATABASE}). Please check to exist it"
@@ -103,3 +114,6 @@ connected_db_name="${TEMPORARY_DATABASE}" \
 connected_db_name="${RESTORE_DATABASE}" \
   target_db_name="${TEMPORARY_DATABASE}" \
   drop_database
+
+# re-initialize data
+re_initialize_data
